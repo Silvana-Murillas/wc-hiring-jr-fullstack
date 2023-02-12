979af799035ba45eae27fe5e1d373749573d9523
Silvana-Murillas <110003502+Silvana-Murillas@users.noreply.github.com>
2023-02-12 17:04:17 -0500

    test jr-fullstack

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from './libs/test.js';
 
 /*
 1. Data manipulation:
@@ -33,12 +33,44 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from 'lodash';
+const source = $t.source(1);
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
-})
+    // Your code goes here
+    const targetData = {
+        balance: 0,
+        income: 0,
+        expenses: 0,
+        byCategories: {
+            Restaurants: 0,
+            Income: 0,
+            Groceries: 0,
+            Rent: 0,
+        },
+    }; // Create an object with the structure of the target data
+
+    source.forEach((element) => {
+        if (element.type === 'income') {
+            targetData.income += element.amount;
+            targetData.byCategories.Income += element.amount;
+        }
+        if (element.type === 'expense') {
+            targetData.expenses += element.amount;
+            if (element.category === 'Restaurants') {
+                targetData.byCategories.Restaurants -= element.amount;
+            }
+            if (element.category === 'Groceries') {
+                targetData.byCategories.Groceries -= element.amount;
+            }
+            if (element.category === 'Rent') {
+                targetData.byCategories.Rent -= element.amount;
+            }
+        }
+    }); // iterate sourcedata asking about the type and category and depends of that, add the information into the targetData
+    targetData.balance = targetData.income - targetData.expenses; // calculate the balance depends of income and expenses
+
+    return targetData; // return the targetData
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +79,19 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
+    //because this functions are asynchronous, we must get the information with await bacause we are workin with async function
+    let ids = await $source.getIds(); // obtain the ids
+
+    ids.forEach(async (element) => {
+        // iterate the array of ids to obtain the values of the obj
+        await $source.getText(element);
+    });
+
     // Your code goes here:
     // 1. Get ids: $source.getIds()
     // 2. Get text for every id: $source.getText(id)
     // 3. Return array of texts
-    return 
-})
+    return $source.getAnswer(); // and return the information of values in an array
+});
